fb6ecb8d58b48188a62cbf56fb0201ea0fa012de
RikelmVoltz <142436269+RikelmVoltz@users.noreply.github.com>
2023-08-28 17:13:51 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+// let nome = prompt("qual é seu nome?");
+//let cor = prompt("qual sua cor favorita?");
+
+//console.log("a cor favorita de " + nome + " é " + cor);
+///console.log(`A cor favorita de ${nome} é ${cor}`);
+
+
+
+// console.log(nome.length);
+// console.log(nome.toLocaleLowerCase());
+
+// console.log(nome.toUpperCase());
+//retira os espaços entre as strings
+// console.log(nome.trim());
+// método para buscar determinado texto dentro de uma string;
+// console.log(nome.includes("cenoura"));
+// troca o texto antigo para o texto novo
+
+
+//arrays
+
+// let listaExercicios = ["elevacao lateral" , "supino" , "voador"];
+// console.log(listaExercicios [0]);
+// console.log(listaExercicios [1]);
+// console.log(listaExercicios[2]);
+// console.log(listaExercicios);
+
+/*
+let cão = ["golden" , "pitbull" , "americanBull" , "buldogue" , "terra nova"];
+let numero = Number(prompt("digite um numero de 0 a 4"));
+console.log(cão[numero]);
+*/
+/*
+const pokemon = ["bulbasauro" , "squirtle" , "charmander"];
+console.log(pokemon.length);
+console.log(pokemon[0].length);
+
+// ------------------------------------------
+
+let seriesBoas = ["breaking bad" , "brooklyn nine-nine"];
+console.log(seriesBoas.includes("breaking bad"));
+console.log(seriesBoas.includes("game of thrones"));
+*/
+
+// let instrumentos = ["violao" , "guitarra" , "bateria"];
+// console.log(instrumentos);
+// instrumentos.push("teclado");
+// console.log(instrumentos);
+// instrumentos.pop();
+// console.log(instrumentos);
+
+// instrumentos.splice(2,2);
+// console.log(instrumentos);
+
+let numeros = ["1" , "2" , "3" , "4" , "5" , "¨6"];
+console.log(numeros.length);
+numeros.push(7);
+console.log(numeros);
+numeros.splice(3,2);
+console.log(numeros);
+console.log(numeros.length);
+
+
+
